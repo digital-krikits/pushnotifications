45d8c62c4a1263f08a92d2abe29f2249de4b8a18
Vasile Goian <vasile.g@digitalkrikits.com>
2015-03-02 12:26:09 +0200

    added basic structure

---
+++ src/Digitalkrikits/Pushnotifications/PushnotificationsServiceProvider.php
@@ -0,0 +1,36 @@
+<?php namespace Digitalkrikits\Pushnotifications;
+
+use Illuminate\Support\ServiceProvider;
+
+class PushnotificationsServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+        $this->mergeConfigFrom(
+            __DIR__.'/../../config/app.php', 'dkpush'
+        );
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+
+}
---
+++ src/config/app.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'android-access-key' => '',
+    'android-url' => 'https://android.googleapis.com/gcm/send',
+
+    'ios-passphrase' => '',
+    'ios-host' => 'ssl://gateway.push.apple.com:2195',
+    'ios-cert' => ''
+
+];
