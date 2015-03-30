ff42851c74dc32b71020464d7c26228e217d03d7
Vasile Goian <vasile.goian@doublebit.net>
2015-03-30 09:57:11 +0300

    Code cleanup APN improvements Warning, untested!

--- src/Digitalkrikits/Pushnotifications/Pushnotifications.php
+++ src/Digitalkrikits/Pushnotifications/Pushnotifications.php
@@ -29,37 +29,109 @@ class Pushnotifications
     private $result = [];
 
     /**
-     * @param array $to
+     * @var array APN response codes
+     */
+    private $apnResonses = [
+        0 => 'No errors encountered',
+        1 => 'Processing error',
+        2 => 'Missing device token',
+        3 => 'Missing topic',
+        4 => 'Missing payload',
+        5 => 'Invalid token size',
+        6 => 'Invalid topic size',
+        7 => 'Invalid payload size',
+        8 => 'Invalid token',
+        255 => 'None (unknown)',
+    ];
+
+    /**
+     * Set the notification message
      * @param string $message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @param array $recipients
+     */
+    public function addRecipients(array $recipients)
+    {
+        foreach ($recipients as $to) {
+            $this->addRecipient($to);
+        }
+    }
+
+    /**
+     * @param array $recipient
+     */
+    public function addRecipient(array $recipient)
+    {
+        if ($recipient['platform'] == 'android') {
+            $this->android[] = $recipient['token'];
+        } else if ($recipient['platform'] == 'ios') {
+            $this->ios[] = $recipient['token'];
+        }
+    }
+
+    /**
      * @param array $data
      */
-    public function send(array $to, $message, array $data = [])
+    public function addData(array $data)
+    {
+        $key = key($data);
+        $this->data[$key] = $data[$key];
+    }
+
+    /**
+     * @param string $key
+     */
+    public function deleteData($key)
+    {
+        unset($this->data[$key]);
+    }
+
+    /**
+     * @param array $data
+     */
+    public function setData(array $data)
     {
         $this->data = $data;
-        $this->message = $message;
+    }
 
-
-        foreach ($to as $recipient) {
-            if ($recipient['platform'] == 'android') {
-                $this->android[] = $recipient['token'];
-            } else if ($recipient['platform'] == 'ios') {
-                $this->ios[] = $recipient['token'];
-            }
+    /**
+     * @param array $recipients
+     * @param string $message
+     * @param array $data
+     * @return array
+     */
+    public function send($recipients = [], $message = '', $data = [])
+    {
+        if (count($recipients)) {
+            $this->addRecipients($recipients);
         }
-
+        if (strlen($message)) {
+            $this->setMessage($message);
+        }
+        if (count($data)) {
+            $this->setData($data);
+        }
         $this->ios = array_unique($this->ios);
         $this->android = array_unique($this->android);
 
         if (count($this->ios)) {
             $this->ios();
         }
-
         if (count($this->android)) {
             $this->android();
         }
         return $this->result;
     }
 
+    /**
+     * Send to android devices
+     */
     private function android()
     {
         $api_access_key = config('dkpush.android-access-key');
@@ -98,9 +170,12 @@ class Pushnotifications
         } catch (\Exception $e) {
         }
         $this->result['android'] = $result;
-        // Log if needed or wanted
     }
 
+    /**
+     * Send to iOS devices
+     * @return bool
+     */
     private function ios()
     {
         $passphrase = config('dkpush.ios-passphrase');
@@ -127,25 +202,23 @@ class Pushnotifications
 
         $payload = json_encode($body);
 
+        $fp = stream_socket_client(
+            $host, $err,
+            $errstr, 60, STREAM_CLIENT_CONNECT | STREAM_CLIENT_PERSISTENT, $ctx);
 
+        if (!$fp) {
+            $this->result['ios'] = 'Could not connect to host';
+            return false;
+        }
 
         foreach ($this->ios as $deviceToken) {
-
-            $fp = stream_socket_client(
-                $host, $err,
-                $errstr, 60, STREAM_CLIENT_CONNECT | STREAM_CLIENT_PERSISTENT, $ctx);
-
-            if (!$fp) {
-                $this->result['ios'][] = 'Could not connect to host';
-                return false;
-            }
 
             $msg = chr(0) . @pack('n', 32) . @pack('H*', $deviceToken) . @pack('n', strlen($payload)) . $payload;
 
             $result = fwrite($fp, $msg, strlen($msg));
 
             if (!$result) {
-                $this->result['ios'][] = 'FAIL: ' . $deviceToken;
+                $this->result['ios'][$deviceToken] = ['status' => 'fail', 'message' => 'Payload couldn\'t be sent'];
             } else {
 
 
@@ -154,49 +227,43 @@ class Pushnotifications
                 $changedStreams = stream_select($read, $null, $null, 0, 2000000);
 
                 if ($changedStreams === false) {
-                    error_log('APN Error: Unabled to wait for a stream availability');
-                    $this->result['ios'][] = 'FAIL: ' . $deviceToken;
+                    $this->result['ios'][$deviceToken] = ['status' => 'fail', 'message' => 'Unable to wait for a stream availability'];
                 } elseif ($changedStreams > 0) {
 
                     $responseBinary = fread($fp, 6);
                     if ($responseBinary !== false || strlen($responseBinary) == 6) {
 
                         if (!$responseBinary) {
-                            $this->result['ios'][] = 'SUCCESS: ' . $deviceToken;
+                            $this->result['ios'][$deviceToken] = ['status' => 'success'];
                             continue;
                         }
 
                         $response = @unpack('Ccommand/Cstatus_code/Nidentifier', $responseBinary);
 
-                        error_log('APN: debugPayload response - ' . print_r($response, true));
-
                         if ($response && $response['status_code'] > 0) {
-                            error_log('APN: debugPayload response - status_code:' . $response['status_code']);
-                            $this->result['ios'][] = 'FAIL: ' . $deviceToken;
+                            $this->result['ios'][$deviceToken] = ['status' => 'fail', 'message' => $this->apnResonses[$response['status_code']]];
                             continue;
                         } else {
                             if (isset($response['status_code'])) {
-                                error_log('APN: debugPayload response - ' . print_r($response['status_code'], true));
-                                $this->result['ios'][] = 'SUCCESS: ' . $deviceToken;
+                                $this->result['ios'][$deviceToken] = ['status' => 'success'];
                             }
                         }
 
                     } else {
-                        error_log("APN: responseBinary = $responseBinary");
-                        $this->result['ios'][] = 'FAIL: ' . $deviceToken;
+                        $this->result['ios'][$deviceToken] = ['status' => 'fail', 'message' => 'Invalid responseBinary: ' . $responseBinary];
                         continue;
                     }
                 } else {
-                    $this->result['ios'][] = 'SUCCESS: ' . $deviceToken;
+                    $this->result['ios'][$deviceToken] = ['status' => 'success'];
                     continue;
                 }
 
 
             }
 
-            fclose($fp);
         }
 
+        fclose($fp);
 
     }
 }
